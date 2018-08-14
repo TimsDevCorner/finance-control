d6e02aa479778ee4360a054192123b850a6b62ff
Tim Wundenberg <timwundenberg@outlook.de>
2018-08-14 22:21:26 +0200

    [issue #2] client add routing, navbar and some components

--- frontend/finance-control-client/src/app/app.module.ts
+++ frontend/finance-control-client/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './component/navigator/navigator.component';
+import { TransactionComponent } from './component/edit-mask/transaction/transaction.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatorComponent,
+    TransactionComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
