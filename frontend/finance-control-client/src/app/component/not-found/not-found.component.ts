48684cc8082f5344e907a88d2ec859be0525a670
Tim Wundenberg <timwundenberg@outlook.de>
2018-08-15 17:59:38 +0200

    [issue #2] client added page not found content

--- frontend/finance-control-client/src/app/component/not-found/not-found.component.ts
+++ frontend/finance-control-client/src/app/component/not-found/not-found.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {take} from 'rxjs/operators';
+import {timer} from '../../../../node_modules/rxjs';
+
 
 @Component({
   selector: 'app-not-found',
@@ -9,7 +12,49 @@ export class NotFoundComponent implements OnInit {
 
   constructor() { }
 
+  public count1: number;
+  public count2: number;
+  public count3: number;
+
+
+  randomNum(): number {
+    return Math.floor(Math.random() * 9) + 1;
+  }
+
   ngOnInit() {
+    let i = 0;
+
+    timer(0, 10).pipe(
+      take(40)).subscribe(x => {
+      if (i > 40) {
+        this.count3 = 4;
+      } else {
+        this.count3 = this.randomNum();
+        i++;
+      }
+    });
+
+
+    timer(0, 10).pipe(
+      take(80)).subscribe(x => {
+      if (i > 80) {
+        this.count2 = 0;
+      } else {
+        this.count2 = this.randomNum();
+        i++;
+      }
+    });
+
+
+    timer(0, 10).pipe(
+      take(100)).subscribe(x => {
+      if (i > 100) {
+        this.count1 = 4;
+      } else {
+        this.count1 = this.randomNum();
+        i++;
+      }
+    });
   }
 
 }
